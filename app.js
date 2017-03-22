1042509668acb7523d2bcf550ae9d7fdc680156b
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2017-03-22 06:21:34 -0300

    Finalizado slider entre os pavimentos

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ var express = require('express')
 var app = express()
 var reload = require('reload')
 var http = require('http')
+const path = require('path');
+
 
 app.use(express.static('public'))
+app.use('/scripts/swiper', express.static(path.join(__dirname, 'node_modules/swiper/dist/js')))
+app.use('/stylesheet/swiper', express.static(path.join(__dirname, 'node_modules/swiper/dist/css')))
 
 var server = http.createServer(app)
 
 reload(server, app)
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Mon167 app listening on port 3000!')
 })
