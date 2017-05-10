150987552af326c138f98e32190d03118030b1a9
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2017-05-09 23:37:08 -0300

    Acrescentado o Skycons.js para depois adicionar o forecast.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 
 app.use(express.static('public'))
+app.use('/scripts/skycons', express.static(path.join(__dirname, 'node_modules/skycons')))
 app.use('/scripts/swiper', express.static(path.join(__dirname, 'node_modules/swiper/dist/js')))
 app.use('/stylesheet/swiper', express.static(path.join(__dirname, 'node_modules/swiper/dist/css')))
 
