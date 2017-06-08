b1fc90b15f70d80fdce9a39d71d77a10702b293f
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2017-06-07 22:43:12 -0300

    Mostrando a temperatua e maxima.

--- src/dark-sky-api.es6.js
+++ src/dark-sky-api.es6.js
@@ -1,14 +1,45 @@
 import DarkSkyApi from 'dark-sky-api';
 
-DarkSkyApi.apiKey = '4e4dc8fd7239369835bb7ec33892980d';
- 
-// optional configuration 
-DarkSkyApi.units = 'si'; // default 'us' 
-DarkSkyApi.language = 'pt'; // default 'en' 
-DarkSkyApi.postProcessor = (item) => { // default null; 
+let temperature = document.getElementById("temperature");
+let minTemperature = document.getElementById("minTemperature");
+let maxTemperature = document.getElementById("maxTemperature");
+
+DarkSkyApi.apiKey = '4e4dc8fd7239369835bb7ec33892980d'; 
+
+DarkSkyApi.units = 'si'; 
+DarkSkyApi.language = 'pt'; 
+DarkSkyApi.postProcessor = (item) => { 
   item.day = item.dateTime.format('ddd');
   return item;
 }
 
-DarkSkyApi.loadCurrent()
-  .then(result => console.log(result));
+let skycons = new Skycons();
+
+DarkSkyApi.loadItAll('alerts,flags,minutely')
+  .then(result => {
+    console.log(result);
+    skycons.add("iWeather", result.currently.icon);
+    
+    temperature.innerHTML = Math.trunc(result.currently.temperature);
+
+    let today = result.daily.data[0];
+    console.log(today);
+
+    if (today !== null)
+    {
+      let vMinTemperature = Math.trunc(today.temperatureMin);
+      let vMaxTemperature = Math.trunc(today.temperatureMax);
+      minTemperature.innerHTML = vMinTemperature;
+      maxTemperature.innerHTML = vMaxTemperature;
+    }
+    else
+    {
+      minTemperature.innerHTML = "?";
+      maxTemperature.innerHTML = "?";
+    }
+  });
+
+skycons.play();
+
+//https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,currently,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884098703_80884
+//https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,daily,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884442473_9121
--- src/dark-sky-api.obj.js
+++ src/dark-sky-api.obj.js
@@ -1,22 +1,47 @@
-'use strict';
+"use strict";
 
-var _darkSkyApi = require('dark-sky-api');
+var _darkSkyApi = require("dark-sky-api");
 
 var _darkSkyApi2 = _interopRequireDefault(_darkSkyApi);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+var temperature = document.getElementById("temperature");
+var minTemperature = document.getElementById("minTemperature");
+var maxTemperature = document.getElementById("maxTemperature");
+
 _darkSkyApi2.default.apiKey = '4e4dc8fd7239369835bb7ec33892980d';
 
-// optional configuration 
-_darkSkyApi2.default.units = 'si'; // default 'us' 
-_darkSkyApi2.default.language = 'pt'; // default 'en' 
+_darkSkyApi2.default.units = 'si';
+_darkSkyApi2.default.language = 'pt';
 _darkSkyApi2.default.postProcessor = function (item) {
-  // default null; 
   item.day = item.dateTime.format('ddd');
   return item;
 };
 
-_darkSkyApi2.default.loadCurrent().then(function (result) {
-  return console.log(result);
+var skycons = new Skycons();
+
+_darkSkyApi2.default.loadItAll('alerts,flags,minutely').then(function (result) {
+  console.log(result);
+  skycons.add("iWeather", result.currently.icon);
+
+  temperature.innerHTML = Math.trunc(result.currently.temperature);
+
+  var today = result.daily.data[0];
+  console.log(today);
+
+  if (today !== null) {
+    var vMinTemperature = Math.trunc(today.temperatureMin);
+    var vMaxTemperature = Math.trunc(today.temperatureMax);
+    minTemperature.innerHTML = vMinTemperature;
+    maxTemperature.innerHTML = vMaxTemperature;
+  } else {
+    minTemperature.innerHTML = "?";
+    maxTemperature.innerHTML = "?";
+  }
 });
+
+skycons.play();
+
+//https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,currently,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884098703_80884
+//https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,daily,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884442473_9121
