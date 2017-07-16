cd0a08aa04c0c89fc1ed73982cf1c5498f8c13b7
rooseveltdossantos <rooseveltdossantos@hotmail.com>
2017-07-16 16:06:16 -0300

    Executando com atualizacao da temperatura e da hora

--- public/scripts/dark-sky-api.js
+++ public/scripts/dark-sky-api.js
@@ -6142,60 +6142,107 @@ module.exports = function (str) {
 })(typeof self !== 'undefined' ? self : this);
 
 },{}],13:[function(require,module,exports){
-"use strict";
+'use strict';
 
-var _darkSkyApi = require("dark-sky-api");
+var _darkSkyApi = require('dark-sky-api');
 
 var _darkSkyApi2 = _interopRequireDefault(_darkSkyApi);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function atualizarTemperaturasDoPainel() {
-  var temperature = document.getElementById("temperature");
-  var minTemperature = document.getElementById("minTemperature");
-  var maxTemperature = document.getElementById("maxTemperature");
-
-  console.log("Iniciando chamada a Dark-Sky-Api");
-
-  try {
-    _darkSkyApi2.default.apiKey = '4e4dc8fd7239369835bb7ec33892980d';
-
-    _darkSkyApi2.default.units = 'si';
-    _darkSkyApi2.default.language = 'pt';
-    _darkSkyApi2.default.postProcessor = function (item) {
-      item.day = item.dateTime.format('ddd');
-      return item;
-    };
-
-    var skycons = new Skycons();
-
-    _darkSkyApi2.default.loadItAll('alerts,flags,minutely').then(function (result) {
-      console.log(result);
-      skycons.add("iWeather", result.currently.icon);
-
-      temperature.innerHTML = Math.trunc(result.currently.temperature);
-
-      var today = result.daily.data[0];
-      console.log(today);
-
-      if (today !== null) {
-        var vMinTemperature = Math.trunc(today.temperatureMin);
-        var vMaxTemperature = Math.trunc(today.temperatureMax);
-        minTemperature.innerHTML = vMinTemperature;
-        maxTemperature.innerHTML = vMaxTemperature;
-      } else {
-        minTemperature.innerHTML = "?";
-        maxTemperature.innerHTML = "?";
+// Polyfill: string.padStart
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+  String.prototype.padStart = function padStart(targetLength, padString) {
+    targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+    padString = String(padString || ' ');
+    if (this.length > targetLength) {
+      return String(this);
+    } else {
+      targetLength = targetLength - this.length;
+      if (targetLength > padString.length) {
+        padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
       }
-    });
+      return padString.slice(0, targetLength) + String(this);
+    }
+  };
+}
 
-    skycons.play();
-  } catch (ex) {
-    console.log('error:', ex.message);
+function atualizarTemperaturasDoPainel() {
+
+  function atualizarTemperatura() {
+    var temperature = document.getElementById("temperature");
+    var minTemperature = document.getElementById("minTemperature");
+    var maxTemperature = document.getElementById("maxTemperature");
+
+    console.log("Iniciando chamada a Dark-Sky-Api");
+
+    try {
+      _darkSkyApi2.default.apiKey = '4e4dc8fd7239369835bb7ec33892980d';
+
+      _darkSkyApi2.default.units = 'si';
+      _darkSkyApi2.default.language = 'pt';
+      _darkSkyApi2.default.postProcessor = function (item) {
+        item.day = item.dateTime.format('ddd');
+        return item;
+      };
+
+      var skycons = new Skycons();
+
+      _darkSkyApi2.default.loadItAll('alerts,flags,minutely').then(function (result) {
+        console.log(result);
+        skycons.add("iWeather", result.currently.icon);
+
+        temperature.innerHTML = Math.trunc(result.currently.temperature);
+
+        var today = result.daily.data[0];
+        console.log(today);
+
+        if (today !== null) {
+          var vMinTemperature = Math.trunc(today.temperatureMin);
+          var vMaxTemperature = Math.trunc(today.temperatureMax);
+          minTemperature.innerHTML = vMinTemperature;
+          maxTemperature.innerHTML = vMaxTemperature;
+        } else {
+          minTemperature.innerHTML = "?";
+          maxTemperature.innerHTML = "?";
+        }
+      });
+
+      skycons.play();
+    } catch (ex) {
+      console.log('error:', ex.message);
+    }
   }
+
+  atualizarTemperatura();
+  setInterval(atualizarTemperatura, 1000 * 60 * 5);
+}
+
+function ligarRelogio() {
+
+  function atualizarDataHora() {
+    try {
+      var diasDaSemana = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'];
+      var mesesDoAno = ['JAN', 'FEV', 'MAR', 'ABR', 'MAIO', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ'];
+      var dia = document.getElementById("dia");
+      var now = new Date();
+      var msgDia = diasDaSemana[now.getDay()] + ', ' + now.getDate() + ' DE ' + mesesDoAno[now.getMonth()] + '/' + now.getFullYear();
+      dia.innerHTML = msgDia;
+      var relogio = document.getElementById("relogio");
+      var msgRelogio = String(now.getHours()).padStart(2, '0') + ':' + String(now.getMinutes()).padStart(2, '0');
+      relogio.innerHTML = msgRelogio;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  setInterval(atualizarDataHora, 1000);
 }
 
 atualizarTemperaturasDoPainel();
+ligarRelogio();
 
 //https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,currently,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884098703_80884
 //https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,daily,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884442473_9121
--- src/dark-sky-api.es6.js
+++ src/dark-sky-api.es6.js
@@ -1,60 +1,112 @@
 import DarkSkyApi from 'dark-sky-api';
 
+// Polyfill: string.padStart
+// https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+if (!String.prototype.padStart) {
+    String.prototype.padStart = function padStart(targetLength,padString) {
+        targetLength = targetLength>>0; //floor if number or convert non-number to 0;
+        padString = String(padString || ' ');
+        if (this.length > targetLength) {
+            return String(this);
+        }
+        else {
+            targetLength = targetLength-this.length;
+            if (targetLength > padString.length) {
+                padString += padString.repeat(targetLength/padString.length); //append to original to ensure we are longer than needed
+            }
+            return padString.slice(0,targetLength) + String(this);
+        }
+    };
+}
+
 function atualizarTemperaturasDoPainel()
 {
-  let temperature = document.getElementById("temperature");
-  let minTemperature = document.getElementById("minTemperature");
-  let maxTemperature = document.getElementById("maxTemperature");
 
-  console.log("Iniciando chamada a Dark-Sky-Api");
+  function atualizarTemperatura() {
+    let temperature = document.getElementById("temperature");
+    let minTemperature = document.getElementById("minTemperature");
+    let maxTemperature = document.getElementById("maxTemperature");
 
-  try 
-  {
-    DarkSkyApi.apiKey = '4e4dc8fd7239369835bb7ec33892980d'; 
+    console.log("Iniciando chamada a Dark-Sky-Api");
 
-    DarkSkyApi.units = 'si'; 
-    DarkSkyApi.language = 'pt'; 
-    DarkSkyApi.postProcessor = (item) => 
-    { 
-      item.day = item.dateTime.format('ddd');
-      return item;
+    try 
+    {
+      DarkSkyApi.apiKey = '4e4dc8fd7239369835bb7ec33892980d'; 
+
+      DarkSkyApi.units = 'si'; 
+      DarkSkyApi.language = 'pt'; 
+      DarkSkyApi.postProcessor = (item) => 
+      { 
+        item.day = item.dateTime.format('ddd');
+        return item;
+      }
+
+      let skycons = new Skycons();
+
+      DarkSkyApi.loadItAll('alerts,flags,minutely')
+        .then(result => {
+          console.log(result);
+          skycons.add("iWeather", result.currently.icon);
+          
+          temperature.innerHTML = Math.trunc(result.currently.temperature);
+
+          let today = result.daily.data[0];
+          console.log(today);
+
+          if (today !== null)
+          {
+            let vMinTemperature = Math.trunc(today.temperatureMin);
+            let vMaxTemperature = Math.trunc(today.temperatureMax);
+            minTemperature.innerHTML = vMinTemperature;
+            maxTemperature.innerHTML = vMaxTemperature;
+          }
+          else
+          {
+            minTemperature.innerHTML = "?";
+            maxTemperature.innerHTML = "?";
+          }
+        });
+
+      skycons.play();
     }
-
-    let skycons = new Skycons();
-
-    DarkSkyApi.loadItAll('alerts,flags,minutely')
-      .then(result => {
-        console.log(result);
-        skycons.add("iWeather", result.currently.icon);
-        
-        temperature.innerHTML = Math.trunc(result.currently.temperature);
-
-        let today = result.daily.data[0];
-        console.log(today);
-
-        if (today !== null)
-        {
-          let vMinTemperature = Math.trunc(today.temperatureMin);
-          let vMaxTemperature = Math.trunc(today.temperatureMax);
-          minTemperature.innerHTML = vMinTemperature;
-          maxTemperature.innerHTML = vMaxTemperature;
-        }
-        else
-        {
-          minTemperature.innerHTML = "?";
-          maxTemperature.innerHTML = "?";
-        }
-      });
-
-    skycons.play();
+    catch(ex)
+    {
+      console.log('error:', ex.message)
+    }
   }
-  catch(ex)
-  {
-    console.log('error:', ex.message)
+
+    atualizarTemperatura();
+    setInterval(atualizarTemperatura, 1000 * 60 * 5);
+}
+
+function ligarRelogio()
+{
+
+  function atualizarDataHora()
+  { 
+    try {
+      let diasDaSemana = ['DOM','SEG','TER','QUA','QUI','SEX', 'SAB'];
+      let mesesDoAno = ['JAN', 'FEV','MAR','ABR','MAIO','JUN','JUL','AGO','SET','OUT','NOV','DEZ'];
+      let dia = document.getElementById("dia");
+      let now = new Date();    
+      let msgDia = diasDaSemana[now.getDay()] + ', ' + now.getDate() + ' DE ' + mesesDoAno[now.getMonth()] + '/' + now.getFullYear();
+      dia.innerHTML = msgDia;    
+      let relogio = document.getElementById("relogio");
+      let msgRelogio = String(now.getHours()).padStart(2, '0') + ':' + String(now.getMinutes()).padStart(2, '0');
+      relogio.innerHTML = msgRelogio;
+      
+    } catch (error) {
+      console.log(error);
+    }   
   }
+
+  setInterval(atualizarDataHora, 1000);
+
 }
 
 atualizarTemperaturasDoPainel();
+ligarRelogio();
 
 //https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,currently,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884098703_80884
 //https://api.darksky.net/forecast/4e4dc8fd7239369835bb7ec33892980d/-26.907443699999998,-49.0853849?exclude=alerts,daily,flags,hourly,minutely&lang=pt&units=si&callback=jsonp_1496884442473_9121
